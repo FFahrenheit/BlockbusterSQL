035d2eaa8043a79fad65195c27a459914c9d8767
Iván L. Murillo <diskman199@gmail.com>
2021-02-01 21:08:24 -0600

    Index form
    Ready

--- src/blockbustersql/BlockbusterSQL.java
+++ src/blockbustersql/BlockbusterSQL.java
@@ -5,6 +5,8 @@
  */
 package blockbustersql;
 
+import views.Index;
+
 /**
  *
  * @author ivan_
@@ -14,8 +16,10 @@ public class BlockbusterSQL {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+        Index index  = new Index();
+        index.setVisible(true);
     }
     
 }
